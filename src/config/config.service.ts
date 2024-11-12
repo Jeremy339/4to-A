e8228e5f76f80e2cdead0ee38230fca071d18491
Jeremy339 <jal.pachoqyavirac.edu.ec>
2024-11-11 19:47:22 -0500

    Bugs

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import * as fs from 'fs'
-import {parse} from "dotenv"
+import * as fs from 'fs';
+import { parse } from 'dotenv';
 
 @Injectable()
 export class ConfigService {
-    private readonly envConfig: {[key:string]:string}
-    constructor(){
-        const isDevelopmentEnv = process.env.NODE_ENV !== 'production'
-        if(isDevelopmentEnv){
-            const envFilePath = __dirname +'/../../.env.development'
-            const existPath = fs.existsSync(envFilePath)
-            if(!existPath){
-                console.log('.env.development no existe DEVELOPMENT')
-                process.exit(0)
-            }
-            this.envConfig=parse(fs.readFileSync(envFilePath))
+    private readonly envConfig: { [key: string]: string };
+
+    constructor() {
+        const env = process.env.NODE_ENV || 'development';
+        const envFilePath = `${__dirname}/../../.env.${env}`;
+        const existsPath = fs.existsSync(envFilePath);
+
+        if (!existsPath) {
+            console.log(`.env.${env} no existe`);
+            process.exit(0);
         }
+
+        this.envConfig = parse(fs.readFileSync(envFilePath));
+        /*
         else{
             const envFilePath= __dirname + '/../../.env.production'
             const existPath = fs.existsSync(envFilePath)
@@ -25,8 +27,13 @@ export class ConfigService {
             this.envConfig=parse(fs.readFileSync(envFilePath))
             process.exit(0)
         }
+            */
     }
-    get(key: string):string{
+
+    get(key: string): string {
         return this.envConfig[key];
     }
 }
+
+
+
---
+++ src/database/migrations/1731371821395-table_dou.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class TableDou1731371821395 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
